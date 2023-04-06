ab02621f6b95c85d0757c65377182aeb74a31671
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-04-06 10:58:45 -0300

    Ajustes

--- analisadorLexico/src/Main.java
+++ analisadorLexico/src/Main.java
@@ -4,16 +4,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Lexer teste = new Lexer("C:/Users/Gabriel/Projetos/MiniPascal-Compiler/Testes/Lexer/teste-sem-erros.txt");
         Token scan = teste.scan();
-        if(scan.getClass().toString().equals("class Word")){
-            Word word = (Word) scan;
-            System.out.println("<"+word.getLexeme()+"\t,\t"+word.tag+">");
-        } else System.out.println(scan);
         while(!(scan.toString().equals("65535"))){
-            scan = teste.scan();
+            
             if(scan.getClass().toString().equals("class Word")){
                 Word word = (Word) scan;
                 System.out.println("<"+word.getLexeme()+"\t,\t"+word.tag+">");
-            } else System.out.println(scan);
+            } else System.out.println("<"+scan+"\t,\t404>");
+
+            scan = teste.scan();
         }
     }
 }
