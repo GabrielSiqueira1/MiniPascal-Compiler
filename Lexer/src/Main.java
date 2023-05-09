8a82bb425a60a2de8786dc2824edb10da31b4a43
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-05-09 16:24:55 -0300

    Sintatico

--- Lexer/src/Main.java
+++ Lexer/src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Lexer teste = new Lexer("../../Testes/Lexer/segundo-teste-extra-erro.txt");
-        Token scan = teste.scan();
-        while(!(scan.toString().equals("65535"))){
-            
-            if(scan.getClass().toString().equals("class Word")){
-                Word word = (Word) scan;
-                System.out.println("<"+word.getLexeme()+"\t,\t"+word.tag+">");
-            } else System.out.println("<"+scan+"\t,\t"+scan.tag+">");
-
-            scan = teste.scan();
-        }
-        System.out.println("Tabela de Simbolos");
-        System.out.println(teste.getWords());
+        Lexer lex = new Lexer("../../Testes/Lexer/segundo-teste-extra-erro.txt");
+        Sintatic sint = new Sintatic(lex);
     }
 }
---
+++ Lexer/src/Sintatic.java
@@ -0,0 +1,89 @@
+public class Sintatic {
+    final int
+    //Palavras reservadas
+    IS = 255,
+    PRG = 256,
+    BEG = 257,
+    END = 258,
+    TYPE = 259,
+    INT = 260,
+    CHAR = 261,
+    BOOL = 262,
+    IF = 263,
+    THEN = 264, 
+    ELSE = 265,
+    REPEAT = 266,
+    UNTIL = 267,
+    WHILE = 268,
+    DO = 269,
+    READ = 270,
+    WRITE = 271,
+    FLOAT = 272,
+
+    //Operadores e pontuação
+    EQ = 288,
+    G = 289,
+    GE = 290,
+    L = 291,
+    LE = 292,
+    NE = 293,
+    SUM = 294,
+    SUB = 295,
+    OR = 296,
+    MULT = 297,
+    DIV = 298,
+    AND = 299,
+    DOT = 300,
+    COLON = 301,
+    SEMICOLON = 302,
+    EXCL = 303,
+
+    //Outros tokens
+    ID = 273,
+    OPENB = 274,
+    CLOSEB = 275,
+    OPENQ = 276,
+    CLOSEQ = 277,
+    OPENP = 278,
+    CLOSEP = 279,
+    ATRIB = 280,
+    CHARCONST = 281,
+    LITERAL = 282;
+
+    int tok = 0;
+    Lexer v;
+
+    public Sintatic(Lexer teste) throws Exception{
+        v = teste;
+        tok = v.scan().tag; 
+    }
+
+    void advance() throws Exception{
+        tok = v.scan().tag;
+    }
+
+    void eat(int t) throws Exception{
+        if(tok == t) advance();
+        else throw new Exception("Token desigual ao caractere esperado!");
+    }
+
+    void simpleExpr() throws Exception{
+        switch(tok){
+            case ID:
+            case INT:
+            case FLOAT:
+            case CHAR:
+            case OPENP: term(); simpleExprPrime(); break;
+            default: throw new Exception("Token inesperado!");
+        }
+    }
+
+    void simpleExprPrime(){
+        
+    }
+
+    void term(){
+
+    }
+
+}
