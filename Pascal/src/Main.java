13f931d163467313bc6b4c39758ef96fe774b080
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-06-03 17:34:56 -0300

    Teste 7 completo

--- Pascal/src/Main.java
+++ Pascal/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Lexer lex = new Lexer("../../Testes/Syntactic/primeiro-teste-extra-sextaCorrecao.txt");
+        Lexer lex = new Lexer("../../Testes/Syntactic/segundo-teste-extra-quartaCorrecao.txt");
         new Syntactic(lex);
     }
 }
