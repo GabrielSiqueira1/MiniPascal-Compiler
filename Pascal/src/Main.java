c276d8c5f226f25bba86a42d485a4238d682f9d2
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-06-27 18:08:14 -0300

    Ajustes

--- Pascal/src/Main.java
+++ Pascal/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Lexer lex = new Lexer("../../Testes/Syntactic/segundo-teste-extra-quartaCorrecao.txt");
+        Lexer lex = new Lexer("Testes/Syntactic/teste5-quintaCorrecao.txt");
         new Syntactic(lex);
     }
 }
